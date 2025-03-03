cc9f9c7373a8846d67f29e3290d3f291ce410d9a
EvanY-74 <138799210+EvanY-74@users.noreply.github.com>
2025-03-03 11:42:10 -0500

    changed middleware to full path

--- index.js
+++ index.js
@@ -29,8 +29,12 @@ let submissions;
     console.log('Done fetching');
 })();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
+// app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
