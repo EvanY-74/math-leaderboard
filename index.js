1a93cc9e130ec1465542a074acf7649c4c29d948
EvanY-74 <evany711@gmail.com>
2025-03-13 08:42:25 -0400

    1.0.0b14

--- index.js
+++ index.js
@@ -170,10 +170,10 @@ function authenticateTokenHelper(token) {
         return { isAuthenticated: false, message: 'no token' };
     }
     
-    console.log(users?.length)
+    console.log('before auth', users?.length)
     try {
         console.log(token);
-        console.log(users?.length)
+        console.log('during auth', users?.length)
         const jwtUser = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = users.find(user => user.username.toLowerCase() === jwtUser.username.toLowerCase());
         if (!user) {
