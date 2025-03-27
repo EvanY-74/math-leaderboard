a69df8fcf7b06f3ed364ec0ad102dbf4e643ceac
EvanY-74 <138799210+EvanY-74@users.noreply.github.com>
2025-03-27 08:53:01 -0400

    log auth status 3

--- index.js
+++ index.js
@@ -368,6 +368,13 @@ io.on('connection', socket => {
     });
 });
 
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);      // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
+
 io.use((socket, next) => {
     const cookies = socket.handshake?.headers?.cookie;
     const token = cookies?.split('; ').find(cookie => cookie.startsWith('accessToken='))?.split('=')[1];
